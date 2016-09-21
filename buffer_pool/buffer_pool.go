c172ce7c9b408c8d105effd1d901352439441c90
buck119br <lcx061125@163.com>
2016-09-21 14:05:01 +0800

    add buffer pool

---
+++ buffer_pool/buffer_pool.go
@@ -0,0 +1,149 @@
+package buffer
+
+import (
+	"bytes"
+	"sync"
+
+	"github.com/astaxie/beego/config"
+)
+
+type BufferPool struct {
+	mutex_                      sync.Mutex
+	busy_flag_                  []bool
+	buffer_                     []*bytes.Buffer
+	buffer_size_                int
+	buffer_pool_max_capacity_   int
+	buffer_pool_enlarge_factor_ int
+	buffer_pool_current_size_   int
+}
+
+// Enlarge the buffer pool by buffer_pool_enlarge_factor
+func (this *BufferPool) enlarge() {
+	CommonLog("Buffer pool needs to be enlarged...")
+
+	// Have to make sure that the capacity of the BufferPool WOULD NOT LARGER
+	// than the max capacity.
+	var enlarge_factor = this.buffer_pool_enlarge_factor_
+	if this.buffer_pool_current_size_+this.buffer_pool_enlarge_factor_ >
+		this.buffer_pool_max_capacity_ {
+		enlarge_factor =
+			this.buffer_pool_max_capacity_ - this.buffer_pool_current_size_
+	}
+
+	for n := 0; n < enlarge_factor; n++ {
+		this.busy_flag_ = append(this.busy_flag_, false)
+		this.buffer_ = append(
+			this.buffer_,
+			bytes.NewBuffer(make([]byte, 0, this.buffer_size_*1024)))
+	}
+
+	this.buffer_pool_current_size_ = len(this.busy_flag_)
+
+	CommonLog(
+		"Buffer pool enlarged and length: ",
+		this.buffer_pool_current_size_)
+}
+
+func NewBufferPool(conf config.Configer) *BufferPool {
+
+	var temp_pool BufferPool
+
+	// Loading parameters
+	temp_pool.buffer_size_ = 40
+	temp_pool.buffer_pool_current_size_ = 100
+	temp_pool.buffer_pool_max_capacity_ = 2000
+	temp_pool.buffer_pool_enlarge_factor_ = 100
+
+	// Buffer pool initialization
+	temp_pool.busy_flag_ = make([]bool, 0, temp_pool.buffer_pool_current_size_)
+	temp_pool.buffer_ = make(
+		[]*bytes.Buffer,
+		0,
+		temp_pool.buffer_pool_current_size_)
+
+	for i := 0; i < temp_pool.buffer_pool_current_size_; i++ {
+		temp_pool.busy_flag_ = append(temp_pool.busy_flag_, false)
+		temp_pool.buffer_ = append(
+			temp_pool.buffer_,
+			bytes.NewBuffer(make([]byte, 0, temp_pool.buffer_size_*1024)))
+	}
+
+	CommonLog(
+		"Buffer Pool initialization finished with capacity: ",
+		len(temp_pool.busy_flag_))
+
+	return &temp_pool
+}
+
+/*
+Get fetch a single buffer from the buffer pool and returns:
+	1. The pointer of a bytes.Buffer;
+	2. The buffer index of the buffer pool.
+Notice:
+	Caller has to keep the second return value for purpose of the Release(i)
+*/
+func (this *BufferPool) Get() (*bytes.Buffer, int) {
+
+	this.mutex_.Lock()
+	defer this.mutex_.Unlock()
+
+	DebugLog("Buffer GET: request.")
+	for i := 0; i < this.buffer_pool_current_size_; i++ {
+		if !this.busy_flag_[i] {
+			this.busy_flag_[i] = true
+			DebugfLog("Buffer GET: NO.%d succeeded.", i)
+			return this.buffer_[i], i
+		}
+	}
+
+	if this.buffer_pool_current_size_ < this.buffer_pool_max_capacity_ {
+		temp_pointer := this.buffer_pool_current_size_
+		this.enlarge()
+
+		this.busy_flag_[temp_pointer] = true
+		DebugfLog("Buffer GET: NO.%d succeeded.", temp_pointer)
+		return this.buffer_[temp_pointer], temp_pointer
+	}
+
+	NormalLog("Warning !!! Buffer pool is full!")
+	DebugLog("Buffer GET: temp buffer succeeded.")
+	return bytes.NewBuffer(make([]byte, 0, this.buffer_size_*1024)),
+		this.buffer_pool_max_capacity_ * 2
+}
+
+/*
+Release put a single buffer back into the buffer pool.
+
+Notice:
+	Entrance parameter of Release() is the SECOND return value of Get()
+	not the FIRST one.
+*/
+func (this *BufferPool) Release(index int) {
+
+	this.mutex_.Lock()
+	defer this.mutex_.Unlock()
+	DebugLog("Buffer RELEASE: request.")
+
+	// Boundary conditions
+	if index < 0 ||
+		(index >= this.buffer_pool_current_size_ &&
+			index < this.buffer_pool_max_capacity_) {
+		ErrorfLog("Buffer RELEASE: index{%d} outrange.", index)
+		return
+	} else if index >= this.buffer_pool_max_capacity_ {
+		NormalfLog("Buffer RELEASE: temp buffer release : %d", index)
+		return
+	}
+
+	// Request to release an empty buffer
+	if !this.busy_flag_[index] {
+		NormalfLog("Buffer RELEASE: buffer{%d} already free.", index)
+		return
+	}
+
+	// Release
+	this.busy_flag_[index] = false
+	this.buffer_[index].Reset()
+	DebugfLog("Buffer RELEASE: NO.%d succeeded.", index)
+	return
+}
---
+++ buffer_pool/buffer_pool_test.go
@@ -0,0 +1,168 @@
+package buffer
+
+import (
+	"bytes"
+	"crypto/md5"
+	"encoding/binary"
+	"flag"
+	"testing"
+
+	"github.com/astaxie/beego/config"
+	"github.com/smartystreets/goconvey/convey"
+)
+
+type testUnit struct {
+	req_num_      int
+	buffer_index_ int
+	buffer_       *bytes.Buffer
+	write_length_ int
+	write_err_    error
+	md5_          [16]byte
+}
+
+func TestBufferPool(t *testing.T) {
+	flag.Lookup("logtostderr").Value.Set("true")
+	// flag.Lookup("log_dir").Value.Set("./")
+	flag.Lookup("v").Value.Set("600")
+	flag.Parse()
+	// defer glog.Flush()
+
+	var chan_counter int
+	test_chan := make(chan testUnit, 30)
+	defer close(test_chan)
+
+	conf, err := config.NewConfig(
+		CONFIG_FILE_PROVIDER,
+		ConfDir()+"app.conf")
+	if err != nil {
+		FatalLog(err)
+	}
+
+	convey.Convey("Buffer pool test: ", t, func() {
+
+		test_pool := NewBufferPool(conf)
+
+		convey.Convey("Buffer pool serial test:", func() {
+
+			// For purpose of boundary condition test,
+			// have to allocate the first half at the beginning
+			for i := 0; i < test_pool.buffer_pool_max_capacity_/2; i++ {
+				test_pool.Get()
+			}
+			test_pool.Release(-1)
+			test_pool.Release(test_pool.buffer_pool_max_capacity_ - 1)
+			for i := test_pool.buffer_pool_max_capacity_ / 2; i < test_pool.buffer_pool_max_capacity_+5; i++ {
+				test_pool.Get()
+			}
+			for i := 0; i < test_pool.buffer_pool_max_capacity_+5; i++ {
+				test_pool.Release(i)
+			}
+			test_pool.Release(0)
+			test_pool.Release(test_pool.buffer_pool_max_capacity_ - 1)
+		})
+
+		convey.Convey("Buffer pool release test: should pass.", func() {
+
+			for i := 0; i < 10; i++ {
+				intToBytes := make([]byte, 4)
+				temp_buffer := make([]byte, 0, test_pool.buffer_size_*1024)
+				binary.BigEndian.PutUint32(intToBytes, uint32(i))
+
+				for x := 0; x < test_pool.buffer_size_*256; x++ {
+					temp_buffer = append(temp_buffer, intToBytes...)
+				}
+				md5_temp := md5.Sum(temp_buffer)
+
+				buffer_, buffer_index_ := test_pool.Get()
+				buffer_.Write(temp_buffer)
+
+				temp_buffer_read := make([]byte, test_pool.buffer_size_*1024)
+
+				n, err := buffer_.Read(temp_buffer_read)
+				convey.So(err, convey.ShouldBeNil)
+				convey.So(n, convey.ShouldEqual, test_pool.buffer_size_*1024)
+				test_pool.Release(buffer_index_)
+				convey.So(
+					md5_temp,
+					convey.ShouldEqual,
+					md5.Sum(temp_buffer_read))
+			}
+		})
+
+		convey.Convey("Buffer pool concurrent test: should pass.", func() {
+
+			for i := 0; i < test_pool.buffer_pool_max_capacity_; i++ {
+				go func(count int) {
+					var send_unit testUnit
+
+					intToBytes := make([]byte, 4)
+					temp_buffer_write := make(
+						[]byte,
+						0,
+						test_pool.buffer_size_*1024)
+					binary.BigEndian.PutUint32(intToBytes, uint32(count))
+
+					for x := 0; x < test_pool.buffer_size_*256; x++ {
+						temp_buffer_write = append(
+							temp_buffer_write,
+							intToBytes...)
+					}
+					send_unit.md5_ = md5.Sum(temp_buffer_write)
+					send_unit.req_num_ = count
+					send_unit.buffer_, send_unit.buffer_index_ =
+						test_pool.Get()
+					send_unit.write_length_, send_unit.write_err_ =
+						send_unit.buffer_.Write(temp_buffer_write)
+					test_chan <- send_unit
+				}(i)
+			}
+
+			for receive_unit := range test_chan {
+				temp_buffer_read := make([]byte, test_pool.buffer_size_*1024)
+
+				convey.So(receive_unit.write_err_, convey.ShouldBeNil)
+				convey.So(
+					receive_unit.write_length_,
+					convey.ShouldEqual,
+					test_pool.buffer_size_*1024)
+
+				n, err := receive_unit.buffer_.Read(temp_buffer_read)
+				convey.So(err, convey.ShouldBeNil)
+				convey.So(n, convey.ShouldEqual, test_pool.buffer_size_*1024)
+
+				md5_temp := md5.Sum(temp_buffer_read)
+				test_pool.Release(receive_unit.buffer_index_)
+				convey.So(md5_temp, convey.ShouldEqual, receive_unit.md5_)
+
+				chan_counter++
+				if chan_counter == test_pool.buffer_pool_max_capacity_ {
+					break
+				}
+			}
+		})
+	})
+}
+
+// For purpose of -benchmem
+func BenchmarkBufferPool(b *testing.B) {
+	conf, err := config.NewConfig(
+		CONFIG_FILE_PROVIDER,
+		ConfDir()+"app.conf")
+	if err != nil {
+		ErrorLog(err)
+	}
+	test_pool := NewBufferPool(conf)
+
+	for i := 0; i < b.N; i++ {
+		test_buffer, index := test_pool.Get()
+		intToBytes := make([]byte, 4)
+		temp_buffer_write := make([]byte, 0, test_pool.buffer_size_*1024)
+		binary.BigEndian.PutUint32(intToBytes, uint32(i))
+
+		for x := 0; x < test_pool.buffer_size_*256; x++ {
+			temp_buffer_write = append(temp_buffer_write, intToBytes...)
+		}
+		test_buffer.Write(temp_buffer_write)
+		test_pool.Release(index)
+	}
+}
